93b5d199e7ae3d6b96f60e09440d2c10df9b943d
James Taylor <james.jm.taylor@gmail.com>
2018-02-17 11:13:55 -0800

    API is hosting images now

--- src/main/kotlin/api/controller/WebController.kt
+++ src/main/kotlin/api/controller/WebController.kt
@@ -7,6 +7,19 @@ import org.springframework.web.bind.annotation.PathVariable
 
 import api.repo.GunRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.RequestMethod
+import com.sun.xml.internal.ws.streaming.XMLStreamWriterUtil.getOutputStream
+import org.apache.catalina.manager.StatusTransformer.setContentType
+import org.springframework.core.io.ClassPathResource
+import org.springframework.http.MediaType
+import org.springframework.util.StreamUtils
+import java.io.IOException
+import javax.servlet.http.HttpServletResponse
+
+
+
+
 
 @RestController
 class WebController {
@@ -16,25 +29,26 @@ class WebController {
 
 	@RequestMapping("/save")
 	fun save(): String {
-//		repository.save(Gun("Jack", "Smith"))
-//		repository.save(Gun("Adam", "Johnson"))
-//		repository.save(Gun("Kim", "Smith"))
-//		repository.save(Gun("David", "Williams"))
-//		repository.save(Gun("Peter", "Davis"))
-//
 		repository.save(Gun("","","","test",0,"",0))
 		return "Stub!"
 	}
-
+    //TODO: Find out why 'name' has a really long space between the last character and the closing quote.
 	@RequestMapping("/findall")
 	fun findAll() = repository.findAll()
-
+    //TODO: Implement id fetch
 	@RequestMapping("/findbyid/{id}")
 	fun findById(@PathVariable id: Long)
-			= repository.findOne(id)
+			= repository.findById(id)
 
-	@RequestMapping("findbylastname/{lastName}")
-	fun findByName(@PathVariable name: String)
-			= repository.findByName(name)
-	
+    @RequestMapping(value = "/photo/{id}", method = arrayOf(RequestMethod.GET), produces = arrayOf(MediaType.IMAGE_JPEG_VALUE))
+    @Throws(IOException::class)
+    fun getImage(@PathVariable id: String): ResponseEntity<ByteArray> {
+        val imgFile = ClassPathResource("images/photo/" + id + ".jpg")
+        val bytes = StreamUtils.copyToByteArray(imgFile.inputStream)
+
+        return ResponseEntity
+                .ok()
+                .contentType(MediaType.IMAGE_JPEG)
+                .body(bytes)
+    }
 }
--- src/main/kotlin/api/repo/GunRepository.kt
+++ src/main/kotlin/api/repo/GunRepository.kt
@@ -8,4 +8,5 @@ import org.springframework.stereotype.Repository
 @Repository
 interface GunRepository : CrudRepository<Gun, Long> {
 	fun findByName(name: String): Iterable<Gun>
+    fun findById(id: Long): Gun
 }
