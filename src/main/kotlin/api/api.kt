52dab77ac91a4e15aae8b24a350a873350d603f2
James Taylor <james.jm.taylor@gmail.com>
2018-04-16 07:46:20 -0700

    Successfully generated jar file

--- src/main/kotlin/api/api.kt
+++ src/main/kotlin/api/api.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-open class KotlinSpringJpaPostgresqlApplication
+open class Api
 
 fun main(args: Array<String>) {
-	SpringApplication.run(KotlinSpringJpaPostgresqlApplication::class.java, *args)
+	SpringApplication.run(Api::class.java, *args)
 }
