6a7a06f53259a432ab29911577ab1712983dd743
James Taylor <james.jm.taylor@gmail.com>
2018-04-22 06:25:55 -0700

    Updated build scripts

--- installJreAndPostgres.sh
+++ installJreAndPostgres.sh
@@ -1,15 +1,22 @@
 #!/bin/bash 
 echo Y | apt-get install default-jre
 echo Y | apt-get install postgresql
+echo Y | apt-get install screen
 echo Y | apt autoremove
+sudo apt-get update
+
 pg_ctlcluster 9.5 main start
 sudo su - postgres -c 'psql -c "CREATE USER jtaylor WITH PASSWORD '\''trire123'\'';"'
 sudo su - postgres -c 'psql -c "ALTER USER jtaylor WITH SUPERUSER;"'
 sudo su - postgres -c 'createdb weg' # drop with `dropdb weg` if necessary
+sudo ufw allow OpenSSH # Stands for Uncomplicated FireWall
+sudo ufw allow 8080/tcp # allow access to Tomcat server
+sudo ufw enable
+
 
 sudo useradd jtaylor
 echo jtaylor:trire123 | chpasswd
-usermod -aG sudo jtaylor  #Gives root privelages to user
+usermod -aG sudo jtaylor  # Gives root privelages to user
 su - jtaylor
 
 # sudo git clone https://github.com/jamesjmtaylor/weg-api.git //This must be a manual step
--- runJarAsService.sh
+++ runJarAsService.sh
@@ -1,6 +1,7 @@
 #!/bin/bash 
-#TODO: Figure out correct order of these instructions.
 cd weg-api/target/
 sudo chown jtaylor spring-jpa-postgresql-spring-boot-0.0.1-SNAPSHOT.jar #(chown stands for `CHange OWNer`)
 sudo chmod 500 spring-jpa-postgresql-spring-boot-0.0.1-SNAPSHOT.jar
-java -jar java -jar spring-jpa-postgresql-spring-boot-0.0.1-SNAPSHOT.jar 
+java -jar spring-jpa-postgresql-spring-boot-0.0.1-SNAPSHOT.jar 
+
+#To run in background, see https://www.digitalocean.com/community/tutorials/how-to-install-and-use-screen-on-an-ubuntu-cloud-server
